b14f46e2ad1a6a7379e6fad0f136d4e2ff9fdbd4
Abhay Joshi <abhay.ashokjoshi@gmail.com>
2025-09-27 20:58:17 +0530

    Added Buzz CRUD Feature File into framework.

--- src/test/java/com/qa/pages/AddEmployeePage.java
+++ src/test/java/com/qa/pages/AddEmployeePage.java
@@ -1,9 +1,13 @@
 package com.qa.pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.qa.util.ElementActions;
 import com.qa.util.WaitMethods;
@@ -11,15 +15,14 @@ import com.qa.util.WaitMethods;
 import cucumber.api.Scenario;
 
 /**
- * @author Abhay This class contains page object of Add Employee
+ * @author Abhay This class has page factory code for PIM page
  *
  */
 public class AddEmployeePage {
-
 	WebDriver driver;
 	Scenario scenario;
 
-	// Page objects repository
+	// Page object repo
 
 	@FindBy(xpath = "//span[text()='PIM']")
 	WebElement pimPageLink;
@@ -39,19 +42,65 @@ public class AddEmployeePage {
 	@FindBy(xpath = "//button[text()=' Save ']")
 	WebElement saveButton;
 
-	// Page class constructor
+	// page repo for search emp
+
+	@FindBy(xpath = "//a[text()='Employee List']")
+	WebElement empListPageLink;  
+
+	@FindBy(xpath = "//label[text()='Employee Name']/following::input[1]")
+	WebElement searchByEmpNamefield;
+
+	@FindBy(xpath = "//button[normalize-space()='Search']")
+	WebElement empSearchButton;
+
+	@FindBy(xpath = "//div[@class='oxd-table-row oxd-table-row--with-border oxd-table-row--clickable']/child::div[3]/child::div[1]")
+	WebElement searchedEmpFandMname;
+
+	// page repo for edit emp
+
+	@FindBy(xpath = "//div[@class='oxd-table-cell-actions']/child::button/i[@class='oxd-icon bi-pencil-fill']")
+	WebElement editEmpButton;
+
+	@FindBy(xpath = "//label[text()='Employee Full Name']/following::input[@name='firstName']")
+	WebElement empfirstName;
+
+	@FindBy(xpath = "//label[text()='Employee Full Name']/following::input[@name='middleName']")
+	WebElement empmiddleName;
+
+	@FindBy(xpath = "//label[text()='Employee Full Name']/following::input[@name='lastName']")
+	WebElement emplastName;
+
+	@FindBy(xpath = "//button[text()=' Save ']")
+	WebElement editandSaveButton;
+
+	// page repo for delete emp
+
+	@FindBy(xpath = "//div[@class='oxd-table-cell-actions']/child::button/i[@class='oxd-icon bi-trash']")
+	WebElement deleteSearchedEmp;
+
+	@FindBy(xpath = "//button[text()=' Yes, Delete ']")
+	WebElement deleteConfirmButton;
+
+	@FindBy(xpath = "//span[text()='No Records Found']")
+	WebElement noRecordFoundTextAfterDelete;
+
+	// page class constructor
+	/**
+	 * @param driver
+	 * @param scenario
+	 */
 	public AddEmployeePage(WebDriver driver, Scenario scenario) {
+
 		this.driver = driver;
 		this.scenario = scenario;
 		PageFactory.initElements(driver, this);
 	}
 
-	// Navigate to PIN Page
+	// page operation methods
 	/**
-	 * Navigating to PIM Page
+	 * 
 	 */
-	public void navigateToPimPage() 
-	{
+	public void navigateToPimPage() {
 		ElementActions.clickElement(driver, pimPageLink, scenario);
 	}
 
@@ -59,14 +108,69 @@ public class AddEmployeePage {
 	 * @param fName
 	 * @param mName
 	 * @param lName
+	 *            This method will add new employee and save it
+	 * @throws InterruptedException
 	 */
-	public void addnewEmployee(String fName, String mName, String lName) 
-	{
+	public void addnewEmployee(String fName, String mName, String lName) throws InterruptedException {
+
 		ElementActions.clickElement(driver, addEmpButton, scenario);
 		WaitMethods.staticWait(5000);
 		ElementActions.sendKeys(driver, firstNameField, scenario, fName);
 		ElementActions.sendKeys(driver, middleNameField, scenario, mName);
 		ElementActions.sendKeys(driver, lastNameField, scenario, lName);
 		ElementActions.clickElement(driver, saveButton, scenario);
+
+	}
+
+	/**
+	 * 
+	 */
+	public void navigateToEmpListPage() {
+		ElementActions.clickElement(driver, empListPageLink, scenario);
+	}
+
+	/**
+	 * @param fName
+	 * @param mName
+	 * @return This method will search the existing employee details
+	 */
+	public String searchEmp(String fName, String mName) {
+		WaitMethods.staticWait(5000);
+		ElementActions.sendKeys(driver, searchByEmpNamefield, scenario, fName + " " + mName);
+		WaitMethods.staticWait(2000);
+		Actions objactions = new Actions(driver);
+		objactions.sendKeys(Keys.ARROW_DOWN).build().perform();
+		WaitMethods.staticWait(2000);
+		ElementActions.clickElement(driver, empSearchButton, scenario);
+		return ElementActions.getText(driver, searchedEmpFandMname, scenario);
+	}
+
+	/**
+	 * @param fnameupendtext
+	 * @param mNameupendtext
+	 * @param lNameupendtext
+	 *            This method will edit the employee details and save it
+	 */
+	public void editEmp(String fnameupendtext, String mNameupendtext, String lNameupendtext) {
+
+		ElementActions.clickElement(driver, editEmpButton, scenario);
+		WaitMethods.staticWait(5000);
+		ElementActions.sendKeys(driver, empfirstName, scenario, fnameupendtext);
+		ElementActions.sendKeys(driver, empmiddleName, scenario, mNameupendtext);
+		ElementActions.sendKeys(driver, emplastName, scenario, lNameupendtext);
+		WaitMethods.staticWait(5000);
+		ElementActions.clickElement(driver, editandSaveButton, scenario);
+
+	}
+
+	/**
+	 * @return This method will delete emp details and return "no record found"
+	 *         msg
+	 */
+	public String deleteUdpateEmp() {
+		ElementActions.clickElement(driver, deleteSearchedEmp, scenario);
+		ElementActions.clickElement(driver, deleteConfirmButton, scenario);
+		WaitMethods.staticWait(5000);
+		return ElementActions.getText(driver, noRecordFoundTextAfterDelete, scenario);
 	}
 }
--- src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
+++ src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
@@ -22,7 +22,7 @@ public class EmployeeCRUD extends Base {
 
 	Scenario scenario;
 	LoginPage objLoginPage;
-	AddEmployeePage objAddEmployeePage;
+	AddEmployeePage objAddEmployeePage;  
 
 	@Before
 	public void logintoApplication(Scenario scenario) {
@@ -32,15 +32,17 @@ public class EmployeeCRUD extends Base {
 	@Given("^Navigate to PIM after log in with Admin user$")
 	public void navigate_to_PIM_after_log_in_with_Admin_user() throws Throwable {
 
-		scenario.write("Starting the application into Browser");
+		scenario.write("Starting the Orange HRM application in browser");
 		driver = initializeWebDriver();
 		WaitMethods.staticWait(5000);
 		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
+
 		scenario.write("Logging in to Orange HRMS APplication");
 		objLoginPage = new LoginPage(driver, scenario);
 		String actualHomePageTitle = objLoginPage.logintoApplication(ReadProperties.getAppUserName(),
 				ReadProperties.getAppPassword());
 		Assert.assertEquals("Dashboard", actualHomePageTitle);
+		;
 		WaitMethods.staticWait(5000);
 		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 
@@ -52,51 +54,109 @@ public class EmployeeCRUD extends Base {
 
 	}
 
+	/**
+	 * @param fName
+	 * @param mName
+	 * @param lName
+	 * @throws Throwable
+	 *             This method will add new employee (by fetching values from
+	 *             feature file)
+	 */
 	@When("^I Add employee with  first name as \"([^\"]*)\" and mname as \"([^\"]*)\" and lName as \"([^\"]*)\"$")
 	public void i_Add_employee_with_first_name_as_and_mname_as_and_lName_as(String fName, String mName, String lName)
 			throws Throwable {
-		scenario.write("Adding new employee");
+
+		scenario.write("Adding new Employee");
 		objAddEmployeePage.addnewEmployee(fName, mName, lName);
 		WaitMethods.staticWait(5000);
 		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 
 	}
 
+	/**
+	 * @param fName
+	 * @param mName
+	 * @param lName
+	 * @throws Throwable
+	 *             This method will search and verify newly added employee
+	 *             details using values fetched from feature file
+	 */
 	@Then("^I  verify employeeAdded in list with  first name as \"([^\"]*)\" and mname as \"([^\"]*)\" and lName as \"([^\"]*)\"$")
-	public void i_verify_employeeAdded_in_list_with_first_name_as_and_mname_as_and_lName_as(String arg1, String arg2,
-			String arg3) throws Throwable {
+	public void i_verify_employeeAdded_in_list_with_first_name_as_and_mname_as_and_lName_as(String fName, String mName,
+			String lName) throws Throwable {
 
-		System.out.println("Selenium Code to be added here!");
-
+		scenario.write(" Searching and verifying employee is added ");
+		objAddEmployeePage.navigateToEmpListPage();
+		Assert.assertEquals(fName + " " + mName, objAddEmployeePage.searchEmp(fName, mName));
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	}
 
+	/**
+	 * @param editEmpDataTable
+	 * @throws Throwable
+	 *             This method will update employee details with the values
+	 *             fetched from feature file and save it
+	 */
 	@When("^I click on Edit button and update below values and save the Data$")
-	public void i_click_on_Edit_button_and_update_below_values_and_save_the_Data(DataTable arg1) throws Throwable {
-
-		System.out.println("Selenium Code to be added here!");
-
-	}
-
-	@Then("^I search the employee and ensure that it is searched using below values$")
-	public void i_search_the_employee_and_ensure_that_it_is_searched_using_below_values(DataTable arg1)
+	public void i_click_on_Edit_button_and_update_below_values_and_save_the_Data(DataTable editEmpDataTable)
 			throws Throwable {
 
-		System.out.println("Selenium Code to be added here!");
+		scenario.write(" Editing the emp information ");
+		objAddEmployeePage.editEmp(editEmpDataTable.raw().get(0).get(1), editEmpDataTable.raw().get(1).get(1),
+				editEmpDataTable.raw().get(2).get(1));
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 
 	}
 
+	/**
+	 * @param editedEmpTablevalue
+	 * @throws Throwable
+	 *             This method will search and verify newly added employee
+	 *             details using values fetched from feature file
+	 */
+	@Then("^I search the employee and ensure that it is searched using below values$")
+	public void i_search_the_employee_and_ensure_that_it_is_searched_using_below_values(DataTable editedEmpTablevalue)
+			throws Throwable {
+
+		scenario.write(" Searching and verifying employee is updated ");
+
+		objAddEmployeePage.navigateToEmpListPage();
+		Assert.assertEquals(editedEmpTablevalue.raw().get(0).get(1) + " " + editedEmpTablevalue.raw().get(1).get(1),
+				objAddEmployeePage.searchEmp(editedEmpTablevalue.raw().get(0).get(1),
+						editedEmpTablevalue.raw().get(1).get(1)));
+
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
+
+	}
+
+	/**
+	 * @throws Throwable
+	 *             This method will search deleted emp deatils
+	 */
 	@Then("^I select and Delete the Updated Employee and verify employee is not  in search result$")
 	public void i_select_and_Delete_the_Updated_Employee_and_verify_employee_is_not_in_search_result()
 			throws Throwable {
 
-		System.out.println("Selenium Code to be added here!");
+		scenario.write(" Deleting Updated Record  ");
 
+		Assert.assertEquals("No Records Found", objAddEmployeePage.deleteUdpateEmp());
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	}
 
+	/**
+	 * @param scenario
+	 * After hook to close the app
+	 */
 	@After
+
 	public void closeApplication(Scenario scenario) {
+
 		scenario.write("Closing the application");
+
 		closeBrowser();
 	}
-
 }
