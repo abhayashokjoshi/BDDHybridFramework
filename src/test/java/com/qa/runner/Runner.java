9bec62d447bdaa04e72aa135e1cb5e821c0770a4
Abhay Joshi <abhay.ashokjoshi@gmail.com>
2025-10-05 09:22:50 +0530

    Adding updated buzz code

--- src/test/java/com/qa/runner/Runner.java
+++ src/test/java/com/qa/runner/Runner.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 // "html:target/cucumber-reports" }, monochrome = true)
 
 @CucumberOptions(features = "src/test/resources/features", glue = "com.qa.stepdefinations", tags = {"@BuzzCRUDTest"}, plugin = { "pretty", "json:target/cucumber-report.json",
-		"json:target/cucumber-reports/Cucumber.json" }, monochrome = true )
+		"json:target/cucumber-reports/Cucumber.json" }, monochrome = true ) 
 
 //json:target/cucumber-reports/Cucumber.json
 //local working code line : //junit:target/cucumber-report.xml
